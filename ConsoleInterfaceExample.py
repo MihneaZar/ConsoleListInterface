1f6a642211a8096bcc5e7ab47e71a4fade4d5091
Mihnea-Bogdan ZAROJANU <mihnea.zarojanu@stud.acs.upb.ro>
2026-04-25 20:05:33 +0300

    Added getItems

--- ConsoleInterfaceExample.py
+++ ConsoleInterfaceExample.py
@@ -1,5 +1,15 @@
-from ConsoleListInterface import ConsoleListInterface, cls, moveCursor
+from ConsoleListInterface import ConsoleListInterface
+from readchar import key
 
+
+def main():
+    console = ConsoleListInterface(open("example.txt").readlines())
+    while True:
+        command, curr_pos = console.interact()
+
+        if command == key.ENTER:
+            console.separateInteraction()
 
 
 if __name__ == "__main__":
+    main()

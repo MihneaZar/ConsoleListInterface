62add22db37e66f5768d22cd9b4d0627d07684cd
Mihnea-Bogdan ZAROJANU <mihnea.zarojanu@stud.acs.upb.ro>
2026-05-02 14:23:32 +0300

    changed to version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ConsoleListInterface',
-    version='0.1.0',    
+    version='1.0.0',    
     description='Simple console interface for interacting with a dynamic list',
     url='https://github.com/MihneaZar/ConsoleListInterface',
     author='Mihnea Bogdan Zarojanu',
